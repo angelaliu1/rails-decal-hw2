aaf2ba3694edf66ce4dcc5326bec32df971d1b51
angelaliu1 <liuangela555@gmail.com>
2018-10-06 16:43:04 -0700

    city.rb, main_controller from lab2"

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,24 +1,19 @@
 class City
-	attr_accessor :name, :landmark, :population, :weather
-	$cities = {}
+	attr_accessor :name, :temperature, :description
+  $cities = []
 
 	def initialize(city_params)
-		@name = city_params[:city]
-		@landmark = city_params[:landmark]
-		@population = city_params[:population]
-		@weather = WeatherService.get(@name)
+		@name = city_params[:name]
+		@temperature = city_params[:temperature]
+		@description = city_params[:description]
 	end
 
-	def save
-		$cities[@name.to_sym] = self
-	end
+  def save
+    $cities << self
+  end
 
-	def self.all
+  def self.all
     $cities
   end
 
-	def update(update_params)
-		# Your code here, for Task 4
-		# Use update_params (a hash) to update the model
-	end
 end
